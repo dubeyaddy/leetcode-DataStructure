53b6cc9fa9880c499227fb847c5c353c8588fbab
adarsh.b.dubey <adarsh.b.dubey@accenture.com>
2026-03-15 12:44:40 +0530

    updates

--- dsa-leetcode/src/main/java/com/practice/dsa/ValidParentheses.kt
+++ dsa-leetcode/src/main/java/com/practice/dsa/ValidParentheses.kt
@@ -1,5 +1,18 @@
 package com.practice.dsa
 
+
+/*
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+
+ */
 fun isValidParentheses(s: String): Boolean {
 
     var stack = ArrayDeque<Char>()
@@ -20,3 +33,5 @@ fun isValidParentheses(s: String): Boolean {
     return stack.isEmpty()
 
 }
+
+//Time complexity is O(n)
